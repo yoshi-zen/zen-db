963e2ca184b942e15f26814e45a79653da4117e3
yoshi-zen <yoshi180705@ruri.waseda.jp>
2025-02-11 16:45:14 +0900

    maven導入

---
+++ src/java/zendb/src/main/java/zendb/Main.java
@@ -0,0 +1,11 @@
+package zendb;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
